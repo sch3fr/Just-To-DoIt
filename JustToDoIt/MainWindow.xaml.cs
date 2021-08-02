7d52c2c2ce42a9fa8021d96ee9e7a0433d4091cf
sch3fr <sch3fr@gmail.com>
2021-08-02 11:44:27 +0200

    programming is a fruitless job u kno xd

--- JustToDoIt/MainWindow.xaml.cs
+++ JustToDoIt/MainWindow.xaml.cs
@@ -17,38 +17,45 @@ using System.Windows.Shapes;
 namespace JustToDoIt
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Basically you write task into the text box on new task page. The Add Task buddon should add it to the list on Task List page.
+    /// On the Task List there are two buttons, one to delete selected task, the other to delete all tasks.
+    /// You should be able to mark tasks as done with the checkbox next to every task.
     /// </summary>
+    
+    //Important Names
+    //TextBox = textBox
+    //Add Task button = buttonAddTask
+    //Task List Box = list
+    //Delete Single Task button = buttonDelOne
+    //Delete All Tasks button = buttonDelAll
     public partial class MainWindow : Window
     {
-        List<TodoItem> taskList = new List<TodoItem>();
-
+        // List<TodoItem> taskList = new List<TodoItem>();
+        public ObservableCollection<string> allTasks { get; set; }
 
         public MainWindow()
         {
-            InitializeComponent();
 
-            list.ItemsSource = taskList;
+            allTasks = new ObservableCollection<string>();
+            this.DataContext = this;
+            InitializeComponent();
+            
+            //list.ItemsSource = taskList;
 
         }
 
+        /*
         public class TodoItem
         {
             public string Title { get; set; }
             public bool Completion { get; set; }
         }
-        
-        /*
-        public ObservableCollection<>
-            {
-
-            }
         */
-
 
         private void buttonAddTask_Click(object sender, RoutedEventArgs e)
         {
-            taskList.Add(new TodoItem() { Title = textBox.Text, Completion = false });
+            //taskList.Add(new TodoItem() { Title = textBox.Text, Completion = false });
+            allTasks.Add(textBox.Text);
             MessageBox.Show("Task " + textBox.Text + " has been added to the list");
         }
 
