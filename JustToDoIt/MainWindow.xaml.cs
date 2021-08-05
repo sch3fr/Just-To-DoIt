42e1997ffbcdd282d68714bfb523f550b18ba939
sch3fr <sch3fr@gmail.com>
2021-08-05 11:47:36 +0200

    added the ability to save the tasks between sessions

--- JustToDoIt/MainWindow.xaml.cs
+++ JustToDoIt/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +31,10 @@ namespace JustToDoIt
     //Delete Single Task button = buttonDelOne
     //Delete All Tasks button = buttonDelAll
     //textblock in list = taskListNames
+
+    /*TODO 
+        move task down the list when checked as done
+    */
     public partial class MainWindow : Window
     {
         public ObservableCollection<string> allTasks { get; set; }
@@ -37,21 +43,42 @@ namespace JustToDoIt
         {
             allTasks = new ObservableCollection<string>();
             this.DataContext = this;
+
+            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<string>));
+            using (StreamReader rd = new StreamReader("AllTasksList.xml"))
+            {
+                allTasks = xs.Deserialize(rd) as ObservableCollection<string>;
+            }
+
             InitializeComponent();
+
+            this.Closed += new EventHandler(MainWindow_Closed); // creates new event when the window is closed
+
+
+            
         }
-        private void buttonAddTask_Click(object sender, RoutedEventArgs e)
+
+        void MainWindow_Closed(object sender, EventArgs e) //saves the tasks when user presses the close button
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<string>));
+            using (StreamWriter wr = new StreamWriter("AllTasksList.xml"))
+            {
+                xs.Serialize(wr, allTasks);
+            }
+        }
+        private void buttonAddTask_Click(object sender, RoutedEventArgs e) //adds tasks from the textBox to the taskList
         {
             allTasks.Add(textBox.Text);
             
             MessageBox.Show("Task " + textBox.Text + " has been added to the list");
         }
 
-        private void buttonDelAll_Click(object sender, RoutedEventArgs e)
+        private void buttonDelAll_Click(object sender, RoutedEventArgs e) //deletes all tasks from the list
         {
             allTasks.Clear();
         }
 
-        private void buttonDelOne_Click(object sender, RoutedEventArgs e)
+        private void buttonDelOne_Click(object sender, RoutedEventArgs e) //removes selected task from the list
         {
             allTasks.Remove((string)list.SelectedItem);
         }
